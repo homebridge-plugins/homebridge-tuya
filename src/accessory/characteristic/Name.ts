e137058f911b50aa444d2e6b15bd4a834026b0bf
tassy-h <98164949+tassy-h@users.noreply.github.com>
2026-05-02 00:17:47 +0900

    Fixed an issue where the name set in the Home app was being reset.
    Fixed an issue where the name set in the Home app was being reset after a restart.
    However, since the plugin cannot retrieve the name configured in the Home app, this fix only avoids the problem by preventing unnecessary updates.
    If the plugin’s cache is cleared, the name on the Home app side will also be reset — this is a known limitation caused by the plugin’s constraints, and unfortunately it cannot be resolved.

--- src/accessory/characteristic/Name.ts
+++ src/accessory/characteristic/Name.ts
@@ -3,15 +3,20 @@ import BaseAccessory from '../BaseAccessory';
 import { sanitizeName } from '../../util/util';
 
 export function configureName(accessory: BaseAccessory, service: Service, name: string) {
-
   const fallbackName = name.replace(/[^A-Za-z0-9 '\s]/g, ' ').replace(/\s+/g, ' ').trim();
   const safeName = sanitizeName(name) ?? (fallbackName || 'Tuya Service');
 
   service.setCharacteristic(accessory.Characteristic.Name, safeName);
   if (!service.testCharacteristic(accessory.Characteristic.ConfiguredName)) {
     service.addOptionalCharacteristic(accessory.Characteristic.ConfiguredName); // silence warning
+    // update every time so cached invalid names get corrected on restart
+    service.setCharacteristic(accessory.Characteristic.ConfiguredName, safeName);
+  } else {
+    // cached Name
+    const currentName = service.getCharacteristic(accessory.Characteristic.ConfiguredName).value;
+    // Since unnecessary value updates were overwriting the name set in the Home app, it will now be updated only once—the first time.
+    if (!currentName) {
+      service.setCharacteristic(accessory.Characteristic.ConfiguredName, safeName);
+    }
   }
-  // update every time so cached invalid names get corrected on restart
-  service.setCharacteristic(accessory.Characteristic.ConfiguredName, safeName);
-
 }
